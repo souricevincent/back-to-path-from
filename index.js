ba838b0b67e11e3d4346930d1a6dd7130ba4a210
Vincent Sourice <v.sourice@purjus.fr>
2018-07-29 17:23:41 +0200

    pathFrom

---
+++ index.js
@@ -0,0 +1,41 @@
+const path = require('path');
+
+const pathToFrom = function (pathTo, fromTheLevel) {
+    let pathNormalized = path.normalize(pathTo);
+
+    //afficher les nb dernier element du array
+    var arrayDirname = pathNormalized.split(path.sep);
+
+    // prototype for delete empty entry in array
+    Array.prototype.unset = function (val) {
+        var index = this.indexOf(val);
+        if (index > -1) {
+            this.splice(index, 1)
+        }
+    };
+
+    // Delete empty entry
+    arrayDirname.unset('');
+
+    const arrayDirnameLength = arrayDirname.length;
+
+    if ( fromTheLevel > 0 && fromTheLevel <= arrayDirnameLength) {
+
+        // traitement
+        // New Path array
+        let newPath = "";
+        let i = arrayDirnameLength - fromTheLevel;
+        while (i < arrayDirnameLength) {
+            newPath = newPath + "/" + arrayDirname[i];
+            i++;
+        }
+    } else {
+        console.log('a misstatement with pathFrom');
+    }
+
+};
+
+module.exports = {
+    pathToFrom,
+};
+
